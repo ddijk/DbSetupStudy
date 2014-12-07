a7a3e75f19f1c117df62f4335f1afc96b855babc
dickdijk <dickdijk@dicks-macbook-pro.fritz.box>
2014-12-07 16:24:26 +0100

    queue that rejects tasks

---
+++ src/test/java/com/mycompany/dbsetupstudy/MyClassTest.java
@@ -0,0 +1,75 @@
+
+package com.mycompany.dbsetupstudy;
+
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.Operations;
+import static com.ninja_squad.dbsetup.Operations.insertInto;
+import static com.ninja_squad.dbsetup.Operations.sequenceOf;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import com.ninja_squad.dbsetup.destination.DriverManagerDestination;
+import com.ninja_squad.dbsetup.operation.Operation;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author dickdijk
+ */
+public class MyClassTest {
+    
+    public MyClassTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+     @Before
+    public void prepare() throws Exception {
+        Operation operation =
+            sequenceOf(
+             Operations.deleteAllFrom("JOB"),
+//                CommonOperations.INSERT_REFERENCE_DATA,
+                insertInto("JOB")
+                    .columns("ID", "NAME", "SCHEDULE")
+                    .values(1L, "AMA", "Amazon")
+                    .values(2L, "PMI", "PriceMinister")
+                    .build());
+          //  DbSetup dbSetup = new DbSetup(new DataSourceDestination(dataSource), operation);
+            // or without DataSource:
+            // DbSetup dbSetup = new DbSetup(new DriverManagerDestination(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD), operation);
+         String TEST_DB_URL="jdbc:derby://localhost:1527/pearl1";
+         String TEST_DB_USER="dick";
+         String TEST_DB_PASSWORD ="dick";
+         String query = "select * from job";
+         
+                 DbSetup dbSetup = new DbSetup(new DriverManagerDestination(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD), operation);
+            dbSetup.launch();
+        }
+    
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+       
+        
+        Assert.assertEquals("My first test", true, false);
+    }
+    
+}
